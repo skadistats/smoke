8f60e32a015ed14e11db704b3b4d081358c5ed88
Joshua Morris <joshua.a.morris@gmail.com>
2014-01-21 05:46:43 -0800

    local caching for dt decoder

--- smoke/replay/decoder/dt.pxd
+++ smoke/replay/decoder/dt.pxd
@@ -3,12 +3,10 @@
 cpdef DTDecoder mk(object recv_table)
 
 
-cdef object cache
-
-
 cdef class DTDecoder(object):
     cdef public object recv_table
     cdef public object by_index
     cdef public object by_recv_prop
+    cdef object cache
 
     cpdef object decode(DTDecoder self, object stream, object prop_list)
--- smoke/replay/decoder/dt.pyx
+++ smoke/replay/decoder/dt.pyx
@@ -6,19 +6,17 @@ cpdef DTDecoder mk(object recv_table):
     return DTDecoder(recv_table)
 
 
-cdef object cache = dict()
-
-
 cdef class DTDecoder(object):
     def __init__(DTDecoder self, object recv_table):
         self.recv_table = recv_table
         self.by_index = []
         self.by_recv_prop = dict()
+        self.cache = dict()
 
         for recv_prop in recv_table:
-            if recv_prop not in cache:
-                cache[recv_prop] = dcdr_rcvprp.mk(recv_prop)
-            recv_prop_decoder = cache[recv_prop]
+            if recv_prop not in self.cache:
+                self.cache[recv_prop] = dcdr_rcvprp.mk(recv_prop)
+            recv_prop_decoder = self.cache[recv_prop]
 
             self.by_index.append(recv_prop_decoder)
             self.by_recv_prop[recv_prop] = recv_prop_decoder
