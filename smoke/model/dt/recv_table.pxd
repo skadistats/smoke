ea7ef0c806ac95dd33ffbf607921add337b9016c
Joshua Morris <joshua.a.morris@gmail.com>
2014-01-31 01:42:46 -0800

    recv table props by identifier

--- smoke/model/dt/recv_table.pxd
+++ smoke/model/dt/recv_table.pxd
@@ -1,6 +1,6 @@
 
 
 cdef class RecvTable(object):
-    cdef public object dt
+    cdef public unicode dt
     cdef public list recv_props
-    cdef object _by_name
+    cdef object _by_identifier
--- smoke/model/dt/recv_table.pyx
+++ smoke/model/dt/recv_table.pyx
@@ -1,16 +1,17 @@
 # cython: profile=False
 
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 from smoke.model.dt cimport prop as mdl_dt_prp
 
 
 cdef class RecvTable(object):
-    def __cinit__(self, dt, recv_props):
-        cdef list priorities
-        cdef int offset = 0
-        cdef int hole, cursor
-        cdef object recv_prop
-        cdef int flagged_changes_often, changes_often
+    def __cinit__(RecvTable self, unicode dt, list recv_props):
+        cdef:
+            list priorities
+            int offset = 0
+            int hole, cursor
+            object recv_prop
+            int flagged_changes_often, changes_often
 
         recv_props = list(recv_props) # copy
         priorities = sorted(set([rp.pri for rp in recv_props] + [64]))
@@ -32,12 +33,25 @@ cdef class RecvTable(object):
 
         self.dt = dt
         self.recv_props = recv_props
+        self._by_identifier = None
 
-    def __init__(self, dt, recv_props):
-        self._by_name = None
+    def __init__(RecvTable self, unicode dt, list recv_props):
+        self._by_identifier = None
 
-    def __len__(self):
+    def __len__(RecvTable self):
         return len(self.recv_props)
 
-    def __iter__(self):
+    def __iter__(RecvTable self):
         return iter(self.recv_props)
+
+    property by_identifier:
+        def __get__(RecvTable self):
+            if self._by_identifier is None:
+                by_identifier = OrderedDict()
+
+                for recv_prop in self:
+                    by_identifier[(recv_prop.src, recv_prop.name)] = recv_prop
+
+                self._by_identifier = by_identifier
+
+            return self._by_identifier
